65f3be270187c91a70e609e5d26cccd18da0cff2
MChTester1 <magdalena.christopher.com>
2021-06-15 20:52:59 -0400

    new uodated test case

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,7 +8,8 @@ public class Main {
       audi.number="111";
       audi.door="3";
       audi.model="A3";
-
+  test cases for the color of the car
+                test case for immediate requirement is being written down
 
     }
 }
